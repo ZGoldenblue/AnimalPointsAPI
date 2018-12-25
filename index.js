ee6faf6aa441d108aee2afa7224862b91d2c11e2
Zack Golden <ZGoldenblue@gmail.com>
2018-12-24 21:43:24 -0600

    Adding new function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var time = require('time');
-exports.handler = (event, context, callback) => {
+exports.handler1 = (event, context, callback) => {
     var currentTime = new time.Date();
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
         body: 'The time in LA is: ' + currentTime.toString(),
     });
+};
+
+exports.handler2 = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello World!'),
+    };
+    return response;
 };
